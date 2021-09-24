5d2f05a496fb7381373b914ca6ebc43082445c7e
leejongbeom <dev.leejb@gmail.com>
2021-09-24 20:46:57 +0900

    lower_bound & upper_bound

---
+++ README.md
@@ -0,0 +1,9 @@
+# About Repository
+
+본 저장소는 C++를 사용하며 정리한 내용을 담고 있습니다.
+
+<br/>
+
+## Contents  
+
+- [lower_bound & upper_bound](bound_func.cpp) / [Notion 정리](https://distinct-bulb-c95.notion.site/Bound-Function-ed3664b42cfd4945b4ce7a8dda48c406)
---
+++ bound_func.cpp
@@ -0,0 +1,20 @@
+#include <iostream>     
+#include <algorithm>    
+#include <vector>
+
+using namespace std;
+
+int main () {
+  int myints[] = {10, 20, 30, 30, 20, 10, 10, 20};
+  vector<int> v(myints, myints + 8);           
+
+  sort(v.begin(), v.end()); // 정렬 [10, 10, 10, 20, 20, 20, 30, 30]
+
+  vector<int>::iterator low, up;
+  low = lower_bound(v.begin(), v.end(), 20);          
+  up = upper_bound(v.begin(), v.end(), 20);                    
+
+  cout << "lower_bound at position " << (low - v.begin()) << '\n'; // 20보다 작지 않은 첫번째 원소의 인덱스 : 3
+  cout << "upper_bound at position " << (up - v.begin()) << '\n'; //  20보다 큰 첫번째 원소의 인덱스 : 6
+
+  return 0;
