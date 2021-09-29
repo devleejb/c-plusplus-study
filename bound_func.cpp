5414f76b2595ea942809a658213fb77ad6c8d688
leejongbeom <dev.leejb@gmail.com>
2021-09-29 15:24:12 +0900

    Modify Indentation

--- bound_func.cpp
+++ bound_func.cpp
@@ -5,16 +5,16 @@
 using namespace std;
 
 int main () {
-  int myints[] = {10, 20, 30, 30, 20, 10, 10, 20};
-  vector<int> v(myints, myints + 8);           
+	int myints[] = {10, 20, 30, 30, 20, 10, 10, 20};
+	vector<int> v(myints, myints + 8);           
 
-  sort(v.begin(), v.end()); // 정렬 [10, 10, 10, 20, 20, 20, 30, 30]
+	sort(v.begin(), v.end()); // 정렬 [10, 10, 10, 20, 20, 20, 30, 30]
 
-  vector<int>::iterator low, up;
-  low = lower_bound(v.begin(), v.end(), 20);          
-  up = upper_bound(v.begin(), v.end(), 20);                    
+	vector<int>::iterator low, up;
+	low = lower_bound(v.begin(), v.end(), 20);          
+	up = upper_bound(v.begin(), v.end(), 20);                    
 
-  cout << "lower_bound at position " << (low - v.begin()) << '\n'; // 20보다 작지 않은 첫번째 원소의 인덱스 : 3
-  cout << "upper_bound at position " << (up - v.begin()) << '\n'; //  20보다 큰 첫번째 원소의 인덱스 : 6
+	cout << "lower_bound at position " << (low - v.begin()) << '\n'; // 20보다 작지 않은 첫번째 원소의 인덱스 : 3
+	cout << "upper_bound at position " << (up - v.begin()) << '\n'; //  20보다 큰 첫번째 원소의 인덱스 : 6
 
-  return 0;
+	return 0;
